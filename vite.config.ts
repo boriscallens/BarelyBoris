26e280e715a399ba7f115e0fca2bbe47d6462f23
BorisCallens <boriscallens@users.noreply.github.com>
2023-02-07 17:57:42 +0100

    enable https on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	server: {
+		https: true
+	},
+	plugins: [basicSsl(), sveltekit()]
 };
 
 export default config;

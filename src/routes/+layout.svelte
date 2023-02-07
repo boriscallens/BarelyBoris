0d70300c7145b18ad17321cb2b5f90a652bd8b41
BorisCallens <boriscallens@users.noreply.github.com>
2023-02-07 18:59:37 +0100

    login logout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,26 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-	import Footer from '$lib/footer/Footer.svelte';
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+  import Footer from "$lib/footer/Footer.svelte";
+  import Header from "$lib/header/Header.svelte";
+  import "../app.css";
 </script>
 
 <Header />
 
 <main>
-	<slot />
+  <slot />
 </main>
 
-<Footer/>
+<Footer />
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>

4b08266a1b60a1a661d2f1bec09f79e454d96ebe
BorisCallens <boriscallens@users.noreply.github.com>
2023-02-10 10:59:57 +0100

    reverting vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import type { UserConfig } from 'vite';
-import { defineConfig } from 'vite'
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-	let config: UserConfig = {
-		plugins: [sveltekit()]
-	};
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
 
-	if (command === 'serve') {
-		config.server = {https: true};
-		config.plugins?.push(basicSsl());
-	}
-
-	return config;
-});
+export default config;

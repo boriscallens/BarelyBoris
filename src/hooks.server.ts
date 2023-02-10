ce6995f4f180369114a829728033b815dca390a0
BorisCallens <boriscallens@users.noreply.github.com>
2023-02-10 11:09:57 +0100

    unreverting viteconfig. Reverting hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,35 +5,35 @@ import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 import Auth0 from "@auth/core/providers/auth0"
 
-export const authorization: Handle = async ({ event, resolve }) => {
-  // Protect any routes under /authenticated
-  if (event.url.pathname.startsWith('/authenticated')) {
-  const session = await event.locals.getSession();
-        if (!session) {
-            throw redirect(303, '/auth');
-        }
-    }
+// export const authorization: Handle = async ({ event, resolve }) => {
+//   // Protect any routes under /authenticated
+//   if (event.url.pathname.startsWith('/authenticated')) {
+//   const session = await event.locals.getSession();
+//         if (!session) {
+//             throw redirect(303, '/auth');
+//         }
+//     }
 
-    // If the request is still here, just proceed as normally
-    return await resolve(event, {
-        transformPageChunk: ({ html }) => html
-    });
-}
+//     // If the request is still here, just proceed as normally
+//     return await resolve(event, {
+//         transformPageChunk: ({ html }) => html
+//     });
+// }
 
 // First handle authentication, then authorization
 // Each function acts as a middleware, receiving the request handle
 // And returning a handle which gets passed to the next function
-export const handle: Handle = sequence(
-  SvelteKitAuth({
-    debug: dev,
-    trustHost: true,
-    providers: [
-      //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
-      Auth0({
-        clientId: AUTH0_CLIENT_ID,
-        clientSecret: AUTH0_CLIENT_SECRET,
-        issuer: AUTH0_ISSUER_BASE_URL
-      }),
-  ]}),
-  authorization,
-);
+// export const handle: Handle = sequence(
+//   SvelteKitAuth({
+//     debug: dev,
+//     trustHost: true,
+//     providers: [
+//       //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
+//       Auth0({
+//         clientId: AUTH0_CLIENT_ID,
+//         clientSecret: AUTH0_CLIENT_SECRET,
+//         issuer: AUTH0_ISSUER_BASE_URL
+//       }),
+//   ]}),
+//   authorization,
+// );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite'
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
+export default defineConfig(({ command, mode, ssrBuild }) => {
+	let config: UserConfig = {
+		plugins: [sveltekit()]
+	};
 
-export default config;
+	if (command === 'serve') {
+		config.server = {https: true};
+		config.plugins?.push(basicSsl());
+	}
+
+	return config;
+});

6e9f20348719e9512f2fd8d04722ced8b79a1078
BorisCallens <boriscallens@users.noreply.github.com>
2023-04-22 19:15:53 +0200

    replaced swa dependencies with vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
-import azure from 'svelte-adapter-azure-swa';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,14 +8,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: azure({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			// pages: 'build',
-			// assets: 'build',
-			// fallback: null,
-			// precompress: false,
-			// strict: true
+		// Consult https://kit.svelte.dev/docs/adapter-vercel for more information
+		adapter: vercel({
 		})
 	}
 };

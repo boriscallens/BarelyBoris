c93f89222b9b7d141c13173a8bc23a81f848feb9
BorisCallens <boriscallens@users.noreply.github.com>
2023-04-22 22:00:34 +0200

    aligned with auth example project

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -13,9 +13,7 @@
 		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}><a href="/">Home</a></li>
-			{#if $page.data.session}
-				<li class:active={$page.url.pathname === '/protected'}><a href="/protected">Protected</a></li>
-			{/if}
+			<li class:active={$page.url.pathname === '/protected'}><a href="/protected">Protected</a></li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />

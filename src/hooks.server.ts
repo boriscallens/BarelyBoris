cbe0cc13a73f1b7fb27356f6d9af7f35361fa964
BorisCallens <boriscallens@users.noreply.github.com>
2023-04-22 21:46:33 +0200

    removed prerender from page

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import Auth0, { type Auth0Profile } from "@auth/core/providers/auth0";
+import Auth0 from "@auth/core/providers/auth0";
 import { AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_ISSUER_BASE_URL } from "$env/static/private";
-import type { Provider } from "@auth/core/providers";
 
 export const handle = SvelteKitAuth({
     //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
@@ -9,5 +8,5 @@ export const handle = SvelteKitAuth({
         clientId: AUTH0_CLIENT_ID,
         clientSecret: AUTH0_CLIENT_SECRET,
         issuer: AUTH0_ISSUER_BASE_URL,
-    }) as Provider<Auth0Profile>]
+    })]
 });

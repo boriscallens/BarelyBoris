9fca0be02af5fbb165fd47c66675087d45753061
BorisCallens <boriscallens@users.noreply.github.com>
2023-05-01 18:19:48 +0200

    minimal navigation

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { Avatar } from '@skeletonlabs/skeleton';
+  import { page } from "$app/stores";
+  import { Avatar, Tab, TabGroup } from "@skeletonlabs/skeleton";
 
-	let signedIn = $page.data.session;
-	let avatarProps = {
-		cursor: "cursor-pointer",
-		border: "border-4 border-surface-300-600-token hover:!border-primary-500",
-		width: "w-12 h-12",
-		rounded: "rounded-full",
-		src: $page.data.session?.user?.image ?? undefined,
-		initials: $page.data.session?.user?.name?.split(' ').map((n) => n[0]).join('') ?? "?",
+  let signedIn = $page.data.session;
+  let avatarProps = {
+    cursor: "cursor-pointer",
+    border: "border-4 border-surface-300-600-token hover:!border-primary-500",
+    width: "w-12 h-12",
+    rounded: "rounded-full",
+    src: $page.data.session?.user?.image ?? undefined,
+    initials:
+      $page.data.session?.user?.name
+        ?.split(" ")
+        .map((n) => n[0])
+        .join("") ?? "?",
+  };
 
-	};
+  const anonymousNavItems = [
+		{ name: "Home", href: "/"}
+	];
+  const authenticatedNavItems = [
+    ...anonymousNavItems,
+    { name: "Protected", href: "/protected" },
+  ];
+  const navItems = signedIn ? authenticatedNavItems : anonymousNavItems;
 </script>
 
 <header class="flex h-14">
-	<div class="flex-none w-14 p-1">
-		<!-- TODO home with logo if I ever get around to making one -->
-	</div>
+  <div class="flex-none w-14 p-1">
+    <!-- TODO home with logo if I ever get around to making one -->
+  </div>
 
-	<nav class="grow text-center">nav</nav>
-	<!-- <nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.url.pathname === '/'}><a href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/protected'}><a href="/protected">Protected</a></li>
+  <nav class="grow text-center">
+    <ul>
+			{#each navItems as navItem}
+				<li class:active={$page.url.pathname === navItem.href}><a href="{navItem.href}">{navItem.name}</a></li>
+			{/each}
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav> -->
+  </nav>
 
-	<div class="flex-none w-18 p-1" data-sveltekit-preload-data="off">
-		{#if !signedIn}
-			<a href="/auth/signin" class="buttonPrimary">Sign in</a>
-		{:else }
-			<a href="/auth/signout" title="sign out">
-				<Avatar {...avatarProps}/>
-			</a>
-		{/if}
-	</div>
+  <div class="flex-none w-18 p-1" data-sveltekit-preload-data="off">
+    {#if !signedIn}
+      <a href="/auth/signin" class="buttonPrimary">Sign in</a>
+    {:else}
+      <a href="/auth/signout" title="sign out">
+        <Avatar {...avatarProps} />
+      </a>
+    {/if}
+  </div>
 </header>

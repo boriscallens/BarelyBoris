413a8c9f5760d4885583e27734857d40c7fb6152
BorisCallens <boriscallens@users.noreply.github.com>
2023-06-02 15:35:05 +0200

    Added a fake auth for dev. Traindev hurray

---
+++ src/lib/Auth/BarelyAuth0.ts
@@ -0,0 +1,11 @@
+import { AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_ISSUER_BASE_URL } from "$env/static/private";
+import type { Provider } from "@auth/core/providers";
+import Auth0, { type Auth0Profile } from "@auth/core/providers/auth0";
+
+export default function BarelyAuth0(): Provider<Auth0Profile> {
+    return Auth0({
+        clientId: AUTH0_CLIENT_ID,
+        clientSecret: AUTH0_CLIENT_SECRET,
+        issuer: AUTH0_ISSUER_BASE_URL,
+    });
+};
---
+++ src/lib/Auth/FakeCredentials.ts
@@ -0,0 +1,20 @@
+import type { Provider } from "@auth/core/providers";
+import Credentials, { type CredentialsConfig } from "@auth/core/providers/credentials";
+import type { User } from "@auth/core/types";
+
+export default function FakeProvider(): Provider {
+    return Credentials({
+        credentials: {
+            username: { label: "Username" },
+            password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials, request): Promise<User | null> {
+            const username: string = credentials.username as string ?? "test";
+            return {
+                id: "-1",
+                name: username,
+                email: `${username}@test.com`,
+            };
+        }
+    })
+};
